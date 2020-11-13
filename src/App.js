17ceb81bdfd94afd7b5c070da27119036c971ab6
Kenny Vincent <vincent.kenny98@gmail.com>
2020-11-13 14:32:51 +0700

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
